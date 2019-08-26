f33c44dbc94ce98f7abad9028e12c2da2e332ab1
Pablo Gonzalez Ramirez <pablo.gonzalez@openpay.mx>
2019-08-26 16:02:13 -0500

    pablo/260819 1602

--- src/App.js
+++ src/App.js
@@ -6,14 +6,19 @@ import Error from "./pages/Error";
 import SingleRoom from "./pages/SimgleRoom";
 import Rooms from "./pages/Rooms";
 import {Route, Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+    <Navbar/>
+    <Switch>
     <Route exact path="/" component={Home}/>
     <Route exact path="/rooms" component={Rooms}/>
-    <Route exact path="/single-room" component={SingleRoom}/>
-    </>
+    <Route exact path="/rooms/:slug" component={SingleRoom}/>
+    <Route component={Error}/>
+    </Switch>
+    </> 
   );
 }
 
